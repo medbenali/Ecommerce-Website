f53028ee09e37c642a20ab286f2d46582f489e59
Mohamed BEN ALI <mohamed.benali@esprit.tn>
2023-08-07 14:02:02 +0100

    Add Cross Origin Annotation and /api

--- backend/src/main/java/com/ecommerce/controller/ProductCategoryController.java
+++ backend/src/main/java/com/ecommerce/controller/ProductCategoryController.java
@@ -4,6 +4,7 @@ import com.ecommerce.entity.Product;
 import com.ecommerce.entity.ProductCategory;
 import com.ecommerce.service.ProductCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 public class ProductCategoryController {
 
     @Autowired
